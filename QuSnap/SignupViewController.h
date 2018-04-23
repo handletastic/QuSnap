622fc584ec5c4f3bccf0d0bd6ad128c84f811b19
Hugo Santos <hdsantos89@gmail.com>
2018-04-24 09:47:17 +1000

    Sign Up View
    - Added Sign Up View
    - Added signupViewController
    - Added signupViewController as reference
    - Added signupButton show-segue to Sign Up View

---
+++ QuSnap/SignupViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignupViewController.h
+//  QuSnap
+//
+//  Created by Hugo Santos on 24/4/18.
+//  Copyright © 2018 Hugo Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignupViewController : UIViewController
+
+@end
---
+++ QuSnap/SignupViewController.m
@@ -0,0 +1,21 @@
+//
+//  SignupViewController.m
+//  QuSnap
+//
+//  Created by Hugo Santos on 24/4/18.
+//  Copyright © 2018 Hugo Santos. All rights reserved.
+//
+
+#import "SignupViewController.h"
+
+@interface SignupViewController ()
+
+@end
+
+@implementation SignupViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
