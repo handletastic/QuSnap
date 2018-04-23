a0430c2fdfc803dc20c8b921e4de36549abab995
Hugo Santos <hdsantos89@gmail.com>
2018-04-23 10:49:57 +1000

    Initial Commit

---
+++ QuSnap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuSnap
+//
+//  Created by Hugo Santos on 23/4/18.
+//  Copyright © 2018 Hugo Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuSnap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuSnap
+//
+//  Created by Hugo Santos on 23/4/18.
+//  Copyright © 2018 Hugo Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
