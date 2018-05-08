43d6a255d1e9cbe2546a7f8882d37339e853e931
Hugo Santos <hdsantos89@gmail.com>
2018-05-08 18:38:14 +1000

    User received snaps on homepage
    Home page displays user's received snaps and sender as a sub.
    God what a pain this way.

--- QuSnap/HomeTableViewController.m
+++ QuSnap/HomeTableViewController.m
@@ -13,6 +13,8 @@
 
 @interface HomeTableViewController ()
 
+@property (strong, nonatomic) NSMutableArray *messagesArray;
+
 @end
 
 @implementation HomeTableViewController
@@ -31,9 +33,16 @@
     FIRDatabaseReference *ref = messagesQuery.ref;
     
     [ref observeEventType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
+        NSDictionary *queryResult = snapshot.value;
+        
+        self.messagesArray = [[NSMutableArray alloc] init];
+        self.messagesArray = [[queryResult allValues] mutableCopy];
+        
+        [self.tableView reloadData]; //reload the data on the table view after fetching the messages array of the user
+        
+        //debug for messages for the currently logged user
         NSDictionary *dict = snapshot.value;
         NSString *key = snapshot.key;
-        
         NSLog(@"get messages dictionary %@ key %@" , dict, key);
     }];
 }
@@ -45,18 +54,44 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 0;
+    if (self.messagesArray == nil || self.messagesArray.count == 0) { //there are no messages to display to the user
+        return 1; //still return a single row cell
+    }
+    
+    return self.messagesArray.count; //return the number of messages on the array to create the right number of cell rows
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"messageCell"]; //subtitle cell style allows to display the senderUsername on the message cell
     
-    // Configure the cell...
+    //loading messages
+    if (self.messagesArray == nil) { //if it is nil, therefore it is loading
+        cell.textLabel.text = @"Loading...";
+        cell.textLabel.textAlignment = NSTextAlignmentCenter;
+        cell.accessoryType = UITableViewCellAccessoryNone;
+        
+        return cell;
+    }
+    // no messages to display to user
+    if (self.messagesArray == 0) { //there are no recipients
+        cell.textLabel.text = @"There are no snaps... yet!";
+        cell.textLabel.textAlignment = NSTextAlignmentCenter; //formatting
+        cell.accessoryType = UITableViewCellAccessoryNone; //formatting
+        
+        return cell;
+    }
+    
+    //gets the matching recipient object for the matching cell
+    NSDictionary *messageInfo = [self.messagesArray objectAtIndex:indexPath.row];
+    cell.textLabel.text = [messageInfo objectForKey:@"messageText"]; //gets the username and injects it on the cell
+    cell.detailTextLabel.text = [messageInfo objectForKey:@"senderUsername"];
+    cell.textLabel.textAlignment = NSTextAlignmentLeft; //formatting
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator; //just to make it a bit more intuitive
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
